1e4a5e325d20c4c191cb2f3b2e5386a58e5d3b15
b211dd <osamasam1998@gmail.com>
2024-01-03 21:14:50 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
+      
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
